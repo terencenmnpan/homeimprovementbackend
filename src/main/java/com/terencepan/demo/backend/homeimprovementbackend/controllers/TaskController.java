944ce4794b108bc9d85b09872ff1ac65f30539f6
Terence <terencetpan@gmail.com>
2021-01-31 22:12:47 -0800

    Project Setup

---
+++ src/main/java/com/terencepan/demo/backend/homeimprovementbackend/controllers/TaskController.java
@@ -0,0 +1,7 @@
+package com.terencepan.demo.backend.homeimprovementbackend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+}
---
+++ src/main/java/com/terencepan/demo/backend/homeimprovementbackend/enums/EnumTasks.java
@@ -0,0 +1,12 @@
+package com.terencepan.demo.backend.homeimprovementbackend.enums;
+
+public enum EnumTasks {
+    ;
+    private final String taskName;
+    private final String taskDescription;
+
+    EnumTasks(String taskName, String taskDescription) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+}
